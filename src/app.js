a3ec6c8b595c24f3e21e97bb0be6b8643ddc3818
MichaelCJoseph <mjoseph.cm@gmail.com>
2017-06-01 11:44:59 -0700

    Handle log out flow

--- src/app.js
+++ src/app.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Card, CardSection, Button, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loggedIn: null
+    };
+  }
+
   componentWillMount() {
     firebase.initializeApp({
       apiKey: 'AIzaSyC5UtTWSVjS1zSjXlbpYmgfIr1eucGQbns',
@@ -14,13 +22,40 @@ class App extends Component {
       storageBucket: 'reactnativeauth-6b3c7.appspot.com',
       messagingSenderId: '747079433276'
     });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <Card>
+            <CardSection>
+              <Button onPress={() => firebase.auth().signOut()}>
+                Log Out
+              </Button>
+            </CardSection>
+          </Card>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner size="large" />;
+    }
   }
 
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
