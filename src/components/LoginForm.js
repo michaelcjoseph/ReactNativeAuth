e67344af83cf8caa7d9238b9e3a50047b7e4ab6e
MichaelCJoseph <mjoseph.cm@gmail.com>
2017-06-01 10:56:46 -0700

    Add front end functionality for login

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
-import { Button, Card, CardSection } from './common';
+import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: ''
+    };
+  }
+
   render() {
     return (
       <Card>
-        <CardSection />
-        <CardSection />
+        <CardSection>
+          <Input 
+            label='Email'
+            placeholder='user@gmail.com'
+            value={this.state.email}
+            onChangeText={email => this.setState({ email })}
+          />
+        </CardSection>
+
+        <CardSection>
+          <Input 
+            secureTextEntry
+            label='Password'
+            placeholder='password'
+            value={this.state.password}
+            onChangeText={password => this.setState({ password })}
+          />
+        </CardSection>
+        
         <CardSection>
           <Button>
             Log In
